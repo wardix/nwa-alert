d0bf62e9bb9b9f54b92d2b2bcb5f0ac89e4cb084
Wardi <wardi@nusa.net.id>
2023-12-14 07:38:10 +0700

    feat: send alert to messenger api

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,42 @@
 import Fastify, { FastifyRequest, FastifyReply, FastifyInstance } from 'fastify'
 import { config } from 'dotenv'
+import axios from 'axios'
 
 config()
 
 const PORT = process.env.PORT! || 3000
 const BASIC_CREDENTIALS = process.env.BASIC_CREDENTIALS! || '[]'
 const PROTECTED_ROUTES = process.env.PROTECTED_ROUTES! || '[]'
+const IGNORED_LABELS = process.env.IGNORED_LABELS! || '[]'
+const MESSENGER_API_URL = process.env.MESSENGER_API_URL!
+const MESSENGER_API_KEY = process.env.MESSENGER_API_KEY!
 
 const protectedRoutes = JSON.parse(PROTECTED_ROUTES)
 const basicCredentials = JSON.parse(BASIC_CREDENTIALS)
+const ignoredLabels = JSON.parse(IGNORED_LABELS)
 
-const validateCredential = async (request: FastifyRequest, reply: FastifyReply) => {
+const sendMessage = async (messageData: {
+  to: string
+  type: string
+  msg: string
+}) => {
+  try {
+    const response = await axios.post(MESSENGER_API_URL, messageData, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Api-Key': MESSENGER_API_KEY,
+      },
+    })
+    console.log('Response:', response.data)
+  } catch (error) {
+    console.error('Error:', error)
+  }
+}
+
+const validateCredential = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
   const credential = request.headers.authorization?.split(' ')[1]
   if (!credential || !basicCredentials.includes(atob(credential))) {
     reply.code(401).send({ error: 'Unauthorized: Basic Credential' })
@@ -18,8 +44,45 @@ const validateCredential = async (request: FastifyRequest, reply: FastifyReply) 
 }
 
 const alertRoute = async (fastify: FastifyInstance) => {
-  fastify.post('/', async (request, reply) => {
-    console.log(request.body)
+  fastify.post('/:id', async (request, reply) => {
+    const data = request.body as any
+    const { id } = request.params as { id: string }
+    const titleMessage = []
+    const bodyMessage = []
+    if (data.status === 'resolved') {
+      titleMessage.push('🌊')
+    } else {
+      for (let i = 0; i < data.alerts.length; i++) {
+        titleMessage.push('🔥')
+        if (i == 2) {
+          break
+        }
+      }
+      if (data.alerts.length > 3) {
+        titleMessage.push('+')
+      }
+    }
+    if (data.commonLabels.hasOwnProperty('object')) {
+      titleMessage.push(` ${data.commonLabels.object}`)
+    }
+    titleMessage.push(` ${data.commonLabels.alertname}`)
+
+    for (const alert of data.alerts) {
+      const messageLine = []
+      for (const label in alert.labels) {
+        if (ignoredLabels.includes(label)) {
+          continue
+        }
+        messageLine.push(`${label}: ${alert.labels[label]}`)
+      }
+      bodyMessage.push(messageLine.join(', '))
+    }
+    const messageData = {
+      to: id,
+      type: 'text',
+      msg: titleMessage.join('') + '\n' + bodyMessage.join('\n'),
+    }
+    sendMessage(messageData)
     reply.send('OK')
   })
 }
